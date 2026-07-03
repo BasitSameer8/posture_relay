84f64c76155394da760a5fe962a249244a04f7ea
basitsameerpro <summerkhan054>
2026-07-03 16:03:00 +0500

    Relay Server

---
+++ api/send.js
@@ -0,0 +1,31 @@
+export default async function handler(req, res) {
+
+    if (req.method !== "POST") {
+        return res.status(405).json({
+            error: "POST only"
+        });
+    }
+
+    const { message } = req.body;
+
+    const token = process.env.BOT_TOKEN;
+    const chatId = process.env.CHAT_ID;
+
+    const telegram = await fetch(
+        `https://api.telegram.org/bot8884638958:AAG3KWTagMlx9cOC8gDDE-dRW2d9Mtvtozg/sendMessage`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                chat_id: 6090201889,
+                text: message
+            })
+        }
+    );
+
+    const data = await telegram.json();
+
+    return res.status(200).json(data);
+}
